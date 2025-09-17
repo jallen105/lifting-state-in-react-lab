049b3d68f0934981481e7a8464fd88f94d489e84
Jonathan <jonathanallen105@gmail.com>
2025-09-17 17:41:38 -0400

    updated no ingredients message to be list item

--- src/components/BurgerStack/BurgerStack.jsx
+++ src/components/BurgerStack/BurgerStack.jsx
@@ -5,7 +5,7 @@ const BurgerStack = ({ingredientList, handleClick}) => {
   return <ul>
     { ingredientList.length > 0? ingredientList.map((ingredient, idx) => (
         <Ingredient ingredient={ingredient} handleClick={handleClick} idx={idx} button={'remove'} />
-    )) : 'No Ingredients' }
+    )) : <li>No Ingredients</li> }
   </ul>
   
 };
